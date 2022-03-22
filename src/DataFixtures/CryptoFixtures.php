1614fae48df6b1024ed0a8618e9a2cadf0f02619
lyamtorres <lyamtorres@hotmail.com>
2022-03-22 14:49:11 +0100

    amélioration de l'affichage de détails

--- src/DataFixtures/CryptoFixtures.php
+++ src/DataFixtures/CryptoFixtures.php
@@ -10,7 +10,7 @@ class CryptoFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 5; $i++) {
             $crypto = new Crypto();
             $crypto->setNom('Bitcoin');
             $crypto->setSymbole('BTC');
@@ -19,6 +19,18 @@ class CryptoFixtures extends Fixture
             $crypto->setQuantite(20000000);
             $crypto->setDate(date_create('2008-10-31'));
             $crypto->setNbLikes(48);
+            $manager->persist($crypto);
+        }
+
+        for ($i = 0; $i < 5; $i++) {
+            $crypto = new Crypto();
+            $crypto->setNom('Ethereum');
+            $crypto->setSymbole('ETH');
+            $crypto->setPrix(2726.66);
+            $crypto->setCategorie('Token');
+            $crypto->setQuantite(113500000);
+            $crypto->setDate(date_create('2015-07-30'));
+            $crypto->setNbLikes(23);
             $manager->persist($crypto);
         }
 
