86917ffeaccba793ddbef0008eb3f1aaa3e4669d
lyamtorres <lyamtorres@hotmail.com>
2022-03-26 21:14:47 +0100

    premier commit de la branche

---
+++ src/Controller/CryptoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Crypto;
+use App\Form\CryptoType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+
+class CryptoController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_crypto")
+     */
+    public function index(): Response
+    {
+        $cryptos = $this->getDoctrine()
+            ->getRepository(Crypto::class)
+            ->findAll();
+
+        return $this->render('crypto/index.html.twig', [
+            'cryptos' => $cryptos,
+        ]);
+    }
+
+    /**
+     * @Route("/details/{nom}", methods={"GET"}, name="crypto_info")
+     *
+     * Affiche les détails d'une cryptomonnaie après avoir cliqué sur l'option 'voir plus'
+     */
+    public function cryptoShow(Crypto $crypto): Response
+    {
+        return $this->render('crypto/details_show.html.twig', [
+            'crypto' => $crypto
+        ]);
+    }
+
+    /**
+     * @Route("/my_cryptos", name="app_my_crypto")
+     */
+    public function myCryptosShow(): Response
+    {
+        $cryptos = $this->getDoctrine()
+            ->getRepository(Crypto::class)
+            ->findBy(array('createur' => $this->getUser()));
+
+        return $this->render('crypto/my_cryptos.html.twig', [
+            'cryptos' => $cryptos,
+        ]);
+    }
+
+    /**
+     * Créer une nouvelle crypto
+     * @isGranted("ROLE_USER")
+     * @Route("/new", name="new_crypto")
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return RedirectResponse|Response
+     */
+    public function create(Request $request, EntityManagerInterface $em) : Response
+    {
+        $user = $this->getUser();
+
+        $crypto = new Crypto();
+        $crypto->setCreateur($user);
+
+        $form = $this->createForm(CryptoType::class, $crypto);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($crypto);
+            $em->flush();
+            return $this->redirectToRoute('app_crypto');
+        }
+        return $this->render('crypto/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+}
